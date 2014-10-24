3183776439becddeaa45633198d964749bbafc5d
Zach Machuca <zachmachuca@gmail.com>
2014-10-23 21:40:05 -0400

    Added Devise Gem

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.4.0'
 
 group :doc do
 gem 'sdoc', '~> 0.4.0',  require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+# Required for Heroku
+# Note to set this to actual host name
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
