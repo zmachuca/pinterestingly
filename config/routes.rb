28e53bd3bae0279bda1a2703947d8a7ce88a2ebf
Zach Machuca <zachmachuca@gmail.com>
2014-10-23 22:46:31 -0400

    Added and cleaned up Pins scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "pages#home"
-
   get "about" => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
